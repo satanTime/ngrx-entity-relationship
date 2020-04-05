127bfec28d9c2dafacb12e7a882366339481f29d
Michael Gusev <m@sudo.eu>
2020-04-05 13:28:43 +0200

    fix: build install the dist to all e2e

--- e2e/store/company/company.actions.ts
+++
@@ -1,5 +0,0 @@
-import {createAction, props} from '@ngrx/store';
-
-import {Company} from './company.model';
-
-export const setCompany = createAction('company', props<{entity: Company}>());
--- e2e/store/user/user.actions.ts
+++
@@ -1,5 +0,0 @@
-import {createAction, props} from '@ngrx/store';
-
-import {User} from './user.model';
-
-export const setUser = createAction('user', props<{entity: User}>());
