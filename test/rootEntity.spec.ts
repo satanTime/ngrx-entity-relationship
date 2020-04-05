f41e1a12123b8268250e0aa34cd99a69c09184bc
Michael Gusev <m@sudo.eu>
2020-04-05 23:26:17 +0200

    test: cases for rootEntity

--- test/rootEntity.spec.ts
+++ test/rootEntity.spec.ts
@@ -1,5 +1,4 @@
 import createSpy = jasmine.createSpy;
-
 import {rootEntity} from '../src';
 
 describe('rootEntity', () => {
@@ -11,5 +10,33 @@ describe('rootEntity', () => {
                 ngrxEntityRelationship: 'rootEntity',
             }),
         );
+    });
+
+    it('returns cached value when rootEntityFlags.disabled is true', () => {
+        pending();
+    });
+
+    it('returns cached value when no related entity has not been changed', () => {
+        pending();
+    });
+
+    it('returns cached value when no related entity set has not been changed', () => {
+        pending();
+    });
+
+    it('returns undefined if the related entity does not exist', () => {
+        pending();
+    });
+
+    it('calls relationships with an incrementing prefix and arguments', () => {
+        pending();
+    });
+
+    it('uses transformer', () => {
+        pending();
+    });
+
+    it('uses transformer after relationships', () => {
+        pending();
     });
 });
