6f6027b405e4bc302d575d342d6e88dc7e679aa5
Michael Gusev <m@sudo.eu>
2020-04-10 23:19:03 +0200

    test: respect of selectId from ngrx/data

--- e2e/angular8/src/app/data/data.ts
+++ e2e/angular8/src/app/data/data.ts
@@ -3,6 +3,12 @@ import {EntityMetadataMap} from '@ngrx/data';
 const data: EntityMetadataMap = {
     Hero: {},
     Villain: {},
+    HeroUuid: {
+        selectId: entity => entity.uuid,
+    },
+    VillainUuid: {
+        selectId: entity => entity.uuid,
+    },
 };
 
 export interface Hero {
@@ -16,6 +22,20 @@ export interface Villain {
     id: string;
     villainName: string;
     heroes?: Hero[];
+}
+
+export interface HeroUuid {
+    uuid: string;
+    heroName: string;
+    villain?: VillainUuid;
+    villainId?: string;
+}
+
+export interface VillainUuid {
+    uuid: string;
+    villainName: string;
+    heroes?: HeroUuid[];
+    hero?: HeroUuid;
 }
 
 const pluralNames = {Hero: 'Heroes'};
