2d19729ef3414009c3d09672a9fc398fedb5a007
Michael Gusev <m@sudo.eu>
2020-04-10 19:25:17 +0200

    test: coverage of childEntitySelector

--- test/relatedEntitySelector.spec.ts
+++ test/relatedEntitySelector.spec.ts
@@ -55,7 +55,7 @@ describe('relatedEntitySelector', () => {
                 name: 'name2',
             },
         };
-        selector('', state, [], entity);
+        selector('randRelatedEntitySelector', state, [], entity);
         expect(entity.parent).toEqual(
             jasmine.objectContaining({
                 rel1: true,
