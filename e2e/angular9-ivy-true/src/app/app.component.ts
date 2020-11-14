5bf3422454c707a004184aa7d597c4ecbda8054c
MG <m@sudo.eu>
2020-11-14 14:47:07 +0100

    build: reporters

--- e2e/angular9-ivy-true/src/app/app.component.ts
+++ e2e/angular9-ivy-true/src/app/app.component.ts
@@ -3,7 +3,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
