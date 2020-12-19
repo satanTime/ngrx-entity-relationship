0b837332d7ba051ef5d4688f1bc83e889e34ceaf
MG <m@sudo.eu>
2020-12-19 20:09:04 +0100

    build(a7): single instance for a7 testing

--- e2e/a7/karma.conf.js
+++ e2e/a7/karma.conf.js
@@ -11,16 +11,10 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
-        },
-        coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../coverage/angular7'),
-            reports: ['html', 'lcovonly', 'text-summary'],
-            fixWebpackSourcePaths: true,
         },
         customLaunchers: {
             ChromeCi: {
